8fba6c3bb2f39e2b3b603342b9094da1207df3ad
nika.movchanskaya <nika.movchanskaya@gmail.com>
2023-05-13 04:02:32 +0200

    package.json

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js',
   },
+  module: {
+    rules: [{
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: 'defaults' }],
+          ],
+        },
+      },
+    }],
+  },
 };
